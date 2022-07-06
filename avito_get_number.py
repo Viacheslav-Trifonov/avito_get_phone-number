d69a052f96414a30946f423f5460c2d28d4718bb
Viacheslav-Trifonov <trifonovvd.mail@yandex.ru>
2022-07-06 17:17:59 +0400

    добавлена первая рабочая версия

---
+++ avito_get_number.py
@@ -0,0 +1,44 @@
+import time
+import pytesseract
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from PIL import Image
+import base64
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract-OCR\tesseract.exe'
+
+link = "https://www.avito.ru/moskva/komnaty/koyko-mesto_165m_v_4-k._1022et._1900668254"
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+browser = webdriver.Chrome(options = options)
+browser.get(link)
+
+def main():
+    try:
+        button_num = browser.find_element(By.XPATH, "//button[@data-marker='item-phone-button/card']")
+        button_num.click()
+        time.sleep(2)
+        image_num = browser.find_element(By.XPATH, "//img[@data-marker='phone-popup/phone-image']")
+        code_img = image_num.get_attribute('src')[22:]
+        decode_img = base64.b64decode(code_img)
+        with open('phone_number.png', 'wb') as f:
+            f.write(decode_img)
+        img = Image.open('phone_number.png')
+        cconfig = '--oem 3 --psm 13'
+        text_number = pytesseract.image_to_string(img, config=cconfig)
+        print(f'НОМЕР ТЕЛЕФОНА ДЛЯ СВЯЗИ: {text_number}')
+        os.remove('phone_number.png')
+
+    except NoSuchElementException:
+        print('НЕОБХОДИМО ВОЙТИ НА САЙТ ИЛИ ЗАРЕГЕСТРИРОВАТЬСЯ')
+
+    finally:
+        time.sleep(2)
+        browser.quit()
+
+if __name__ == '__main__':
+    main()
